2f09612ff5fb5270448530d582b388e6783c427a
AbismoDev <contato.abismodev@gmail.com>
2024-02-27 10:04:39 -0300

    update

--- Assets/Scripts/EntityStats.cs
+++ Assets/Scripts/EntityStats.cs
@@ -6,16 +6,4 @@ public class EntityStats : MonoBehaviour
 {
 
     public float base_speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,12 +10,6 @@ public class ScoreManager : MonoBehaviour
 
     public int score_;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
